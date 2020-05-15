778ef6ec5e7dbd2ff9a598206a9e86e235841f1d
PacharapongTas <yen.yamato@gmail.com>
2020-05-15 14:47:23 +0700

    complete thrid

--- ios/ChatBeeTests/ChatBee.m
+++ ios/ChatBeeTests/ChatBee.m
@@ -14,11 +14,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface WirtualTests : XCTestCase
+@interface ChatBeeTests : XCTestCase
 
 @end
 
-@implementation WirtualTests
+@implementation ChatBeeTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
